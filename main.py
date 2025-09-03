7db4f68456cb4e092b85223df18cee6835e8105c
rickarockFR <69355533+rickarockFR@users.noreply.github.com>
2025-09-03 18:44:04 +0200

    haha

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ def get_image(x,y):
 
 files = os.listdir()
 i = 1
-while f'{i}-1.png' in files:
+while f'backups/{i}-1.png' in files:
     i+=1
 
-with open(f'{i}-1.png', 'wb') as f:
+with open(f'backups/{i}-1.png', 'wb') as f:
     f.write(get_image(1189,1174))
-with open(f'{i}-2.png', 'wb') as f:
+with open(f'backups/{i}-2.png', 'wb') as f:
     f.write(get_image(1190,1174))
-with open(f'{i}.txt', 'w') as f:
+with open(f'backups/{i}.txt', 'w') as f:
     f.write(strftime("%H:%M", gmtime()))
 

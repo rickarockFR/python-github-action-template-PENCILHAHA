ebdf283265b5f1d930142736ca4bca278809485a
rickarockFR <69355533+rickarockFR@users.noreply.github.com>
2025-09-03 17:47:54 +0200

    haha

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
-import logging
-import logging.handlers
+import requests
 import os
 
-import requests
+def get_image(x,y):
+    img_data = requests.get(f'https://backend.wplace.live/files/s0/tiles/{x}/{y}.png').content
+    while b'The owner of this website (backend.wplace.live) has banned you temporarily from accessing this website.' in img_data:
+        print('rate')
+        time.sleep(12)
+        print('rate-end')
+        img_data = requests.get(f'https://backend.wplace.live/files/s0/tiles/{x}/{y}.png').content
+    if not b'404 Not Found' in img_data:
+        return img_data
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+files = os.listdir()
+i = 1
+while f'{i}.png' in files:
+    i+=1
 
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
+with open(f'{i}.png', 'w') as f:
+    f.write(get_image(1189,1174))
 
-
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
-
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
